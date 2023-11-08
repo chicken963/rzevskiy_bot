0a074491ef4729f1144e323f581ea1cee306e44d
chicken963 <chazy1976@mail.ru>
2023-11-09 01:01:25 +0400

    added voice_response to Sanya

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import csv
+import random
 
 import yaml
 from telegram.ext import MessageHandler, filters, Application
 
 import re
 import logging
+
 
 logging.basicConfig(
     format='%(asctime)s [%(levelname)s] - %(name)s - %(message)s',
@@ -17,6 +19,9 @@ def read_config():
         return yaml.safe_load(file)
 
 
+replies = 0
+
+
 async def process_full_message_triggers(update):
     text = update.message.text
     pattern_with_optional_punctuation = "{}([!?.,;:\"'])*$"
@@ -27,6 +32,8 @@ async def process_full_message_triggers(update):
             pattern = pattern_with_optional_punctuation.format(trigger)
             if re.match(pattern, text, re.IGNORECASE):
                 reply = row[1]
+                global replies
+                replies += 1
                 await update.message.reply_text(text=reply, quote=True)
 
 
@@ -36,14 +43,29 @@ async def process_snippet_message_triggers(update):
         reader = csv.reader(file)
         for row in reader:
             trigger = row[0]
-            if trigger in text:
+            if re.search(trigger, text, re.IGNORECASE):
                 reply = row[1]
+                global replies
+                replies += 1
                 await update.message.reply_text(text=reply, quote=True)
 
 
+async def reply_to_sanya(update):
+    message = update.message
+    sanya_pattern_eng = "(^Ale(x|ks)and(e?r$|ro)$)|(^San(dro|[yi]a|[yi]ok)$)|(^Sash(k?a|ok)$)|(^Shur(a|ic*k)$)"
+    sanya_pattern_rus = "(^Александр$)|(^Саш[ау].*)|(^Сан(я([гр]а)*|[её]к)$)|(^Шур(а|ик)$)"
+    message_sender_name = message.from_user.first_name
+    if re.match(sanya_pattern_eng, message_sender_name, re.IGNORECASE) or re.match(sanya_pattern_rus, message_sender_name, re.IGNORECASE):
+        await message.reply_voice("sanya.mp3", quote=True)
+
+
 async def message_handler(update, context):
+    global replies
+    replies = 0
     await process_full_message_triggers(update)
     await process_snippet_message_triggers(update)
+    if replies == 0 and random.randint(1, 4) == 1:
+        await reply_to_sanya(update)
 
 
 def main():
